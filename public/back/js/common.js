709fa04df2bdda8d8841f64928f17d41e4c4dcc6
liao <1102702878@qq.com>
2019-08-30 14:48:53 +0800

    登陆功能完成

---
+++ public/back/js/common.js
@@ -0,0 +1,18 @@
+//进度条
+
+//禁用小圆环
+NProgress.configure({ showSpinner: false });
+
+// ajaxStart 所有的 ajax 开始调用
+$(document).ajaxStart(function() {
+  NProgress.start();
+});
+
+
+// ajaxStop 所有的 ajax 结束调用
+$(document).ajaxStop(function() {
+  // 模拟网络延迟
+  setTimeout(function() {
+    NProgress.done();
+  }, 500)
+});
